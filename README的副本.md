9b74d96af601be6bffa5d0a4a19545c6f206ac25
ludanjun <76891972@qq.com>
2024-10-09 12:37:05 +0800

    创建项目

---
+++ README的副本.md
@@ -0,0 +1,3 @@
+# flutter_jdshop
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_jdshop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_jdshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
